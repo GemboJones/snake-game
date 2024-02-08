7c8f9d66755118a2e5510efad814a24118f94463
GemboJones <gembojones@gmail.com>
2024-02-08 20:37:34 +0000

    adds food to game board

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const board = document.getElementById("game-board");
 
-let snake = [{x: 10, y: 10}]
+const gridSize = 20;
+let snake = [{x: 10, y: 10}];
+let food = generateFood();
 
-const draw = ()=>{
+function draw() {
     board.innerHTML=''
     drawSnake()
+    drawFood()
 }
 
-const drawSnake = () => {
+function drawSnake() {
     snake.forEach((segment)=>{
         const snakeElement = createGameElement('div', 'snake')
         setPosition(snakeElement, segment);
@@ -15,15 +18,27 @@ const drawSnake = () => {
     })  
 };
 
-const createGameElement = (tag, className) => {
+function createGameElement(tag, className) {
     const element = document.createElement(tag)
     element.className = className
     return element
 }
 
-const setPosition = (element, position)=>{
+function setPosition(element, position) {
     element.style.gridColumn = position.x
     element.style.gridRow = position.y
 }
 
 draw()
+
+function drawFood () {
+    const foodElement = createGameElement('div', 'food')
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
+}
