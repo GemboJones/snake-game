1b6984541c5a23b9728aa8b9447d35b5a4db614c
GemboJones <gembojones@gmail.com>
2024-02-09 16:25:07 +0000

    adds snake keyboard controls and increasing snake speed

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const board = document.getElementById("game-board");
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
 
 function draw() {
   board.innerHTML = "";
@@ -29,7 +35,7 @@ function setPosition(element, position) {
   element.style.gridRow = position.y;
 }
 
-draw();
+// draw();
 
 function drawFood() {
   const foodElement = createGameElement("div", "food");
@@ -41,4 +47,92 @@ function generateFood() {
   const x = Math.floor(Math.random() * gridSize) + 1;
   const y = Math.floor(Math.random() * gridSize) + 1;
   return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "left":
+      head.x--;
+      break;
+  }
+
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    increaseSpeed()
+    clearInterval(gameInterval);
+    gameInterval = setInterval(() => {
+      move();
+      // checkCollision()
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// setInterval(() => {
+//     move()
+//     draw()
+// }, 200);
+
+function startGame() {
+  gameStarted = true;
+  instructionText.style.display = "none";
+  logo.style.display = "none";
+  gameInterval = setInterval(() => {
+    move();
+    // checkCollision()
+    draw();
+  }, gameSpeedDelay);
+}
+
+function handleKeyPress(event) {
+  if (
+    (!gameStarted && event.code === "Space") ||
+    (!gameStarted && event.key === " ")
+  ) {
+    startGame();
+  } else {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "up";
+        break;
+      case "ArrowDown":
+        direction = "down";
+        break;
+      case "ArrowLeft":
+        direction = "left";
+        break;
+      case "ArrowRight":
+        direction = "right";
+        break;
+    }
+  }
+}
+
+document.addEventListener('keydown', handleKeyPress)
+
+function increaseSpeed () {
+   console.log(gameSpeedDelay);
+   if (gameSpeedDelay > 150) {
+     gameSpeedDelay -= 5;
+   } else if (gameSpeedDelay > 100) {
+     gameSpeedDelay -= 3;
+   } else if (gameSpeedDelay > 50) {
+     gameSpeedDelay -= 2;
+   } else if (gameSpeedDelay > 25) {
+     gameSpeedDelay -= 1;
+   }
 }
